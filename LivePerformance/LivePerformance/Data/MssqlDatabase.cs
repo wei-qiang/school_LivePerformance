1ca149b429c0cc6fb2648276d70476a18fbf1792
wei-qiang <wei-qiang@hotmail.nl>
2017-01-17 21:59:42 +0100

    added project

---
+++ LivePerformance/LivePerformance/Data/MssqlDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivePerformance.Data
+{
+    public abstract class MssqlDatabase
+    {
+        protected const string Connstring = @"Data Source=mssql.fhict.local;Initial Catalog=dbi344145;Integrated Security=False;User ID=dbi344145;Password=liveperformance;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
