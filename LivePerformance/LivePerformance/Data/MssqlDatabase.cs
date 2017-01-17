e567d209cb378f09d88372306b261b415cf44919
wei-qiang <wei-qiang@hotmail.nl>
2017-01-17 22:02:24 +0100

    test

--- LivePerformance/LivePerformance/Data/MssqlDatabase.cs
+++ LivePerformance/LivePerformance/Data/MssqlDatabase.cs
@@ -9,5 +9,5 @@ namespace LivePerformance.Data
     public abstract class MssqlDatabase
     {
         protected const string Connstring = @"Data Source=mssql.fhict.local;Initial Catalog=dbi344145;Integrated Security=False;User ID=dbi344145;Password=liveperformance;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-    }
+    } 
 }
